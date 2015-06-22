bb85290e06f8f69a3bc0b1f65967d735e04187c4
Miguel Mendoza <miguel.mendoza@vizzuality.com>
2015-06-22 15:13:37 +0200

    adds serializer cache

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,8 +4,8 @@ Rails.application.configure do
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
-  config.cache_classes = false
-  #config.cache_store = :dalli_store
+  config.cache_classes = true
+  config.cache_store = :dalli_store
 
   # Do not eager load code on boot.
   config.eager_load = false
