e9bc385e9f9f1a1c1b6c394dddf37592bfc08cb1
Miguel Mendoza <info@miguelmendoza.dj>
2015-06-23 16:35:56 +0200

    sectors refactor

--- app/controllers/concerns/projects_filtering.rb
+++ app/controllers/concerns/projects_filtering.rb
@@ -6,14 +6,14 @@ module ProjectsFiltering
     before_action :get_projects,  only: [:home, :show]
   end
   def get_projects
-    # timestamp = Project.order('updated_at desc').first.updated_at.to_s
-    # string = timestamp + projects_params.inspect
-    # digest = Digest::SHA1.hexdigest(string)
+    timestamp = Project.order('updated_at desc').first.updated_at.to_s
+    string = timestamp + projects_params.inspect
+    digest = Digest::SHA1.hexdigest(string)
     m = ActiveModel::Serializer::ArraySerializer.new(Project.fetch_all(projects_params), each_serializer: ProjectSerializer)
-    @map_data = ActiveModel::Serializer::Adapter::JsonApi.new(m, include: ['organization', 'sectors', 'donors', 'countries', 'regions']).to_json
+    @map_data = Rails.cache.fetch("map_data_projects_#{digest}", :expires_in => 24.hours) {ActiveModel::Serializer::Adapter::JsonApi.new(m, include: ['organization', 'sectors', 'donors', 'countries', 'regions']).to_json}
     @map_data_max_count = 0;
-    @projects_count = Project.fetch_all(projects_params).uniq.length
-    @projects = Project.fetch_all(projects_params).page(params[:page]).per(10)
+    @projects_count = Rails.cache.fetch("projects_count_#{digest}", :expires_in => 24.hours) {Project.fetch_all(projects_params).uniq.length}
+    @projects = Rails.cache.fetch("projects_#{digest}", :expires_in => 24.hours) {Project.fetch_all(projects_params).page(params[:page]).per(10)}
   end
   private
   def projects_params
@@ -22,7 +22,7 @@ module ProjectsFiltering
   def merge_params
     params.merge!({organizations: [params[:id]]}) if controller_name == 'organizations'
     params.merge!({donors: [params[:id]]}) if controller_name == 'donors'
-    params.merge!({sectors: [params[:id]]}) if controller_name == 'sectors'
+    params.merge!({sectors: [params[:id]]}) if controller_name == 'clusters_sectors'
     params.merge!({countries: [params[:id]]}) if controller_name == 'countries'
   end
 end
