9890510b88edcb52e5febc887b4ceedca2ca321d
Miguel Mendoza <info@miguelmendoza.dj>
2015-09-04 12:38:50 +0200

    changes locations to meta

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -1,16 +1,10 @@
 module Api
   module V1
-    require 'digest/sha1'
     class ProjectsController < ApiController
       def index
-        @projects = Project.fetch_all(projects_params)
-        timestamp = @projects.order('projects.updated_at desc').first.try('projects.updated_at').to_s
-        string = timestamp + projects_params.inspect
-        digest = Digest::SHA1.hexdigest(string)
-        m = ActiveModel::Serializer::ArraySerializer.new(@projects, each_serializer: ProjectSerializer)
-        @map_data = Rails.cache.fetch("map_data_projects_#{digest}", :expires_in => 24.hours) {ActiveModel::Serializer::Adapter::JsonApi.new(m, include: ['organization', 'sectors', 'donors', 'geolocations']).to_json}
+        @projects = Project.fetch_all(project_params)
         respond_to do |format|
-         format.json {render json: @map_data}
+         format.json {render json: @projects, root: 'data', include: ['organization', 'sectors', 'donors', 'geolocations']}
          format.xml {@projects}
         end
       end
@@ -24,7 +18,7 @@ module Api
       end
 
 
-      def projects_params
+      def project_params
         params.permit(:offset, :limit, :format, :geolocation, :level, organizations:[], sectors:[], donors:[], countries:[])
       end
     end
--- app/controllers/concerns/projects_filtering.rb
+++ app/controllers/concerns/projects_filtering.rb
@@ -10,8 +10,8 @@ module ProjectsFiltering
     string = timestamp + projects_params.inspect
     digest = Digest::SHA1.hexdigest(string)
     results = Project.fetch_all(projects_params, false)
-    m = ActiveModel::Serializer::ArraySerializer.new(results[0], each_serializer: ProjectSerializer)
-    @map_data = Rails.cache.fetch("map_data_projects_#{digest}", :expires_in => 24.hours) {[ActiveModel::Serializer::Adapter::JsonApi.new(m, include: ['organization', 'sectors', 'donors', 'geolocations']).to_json, results[1].to_json]}
+    m = ActiveModel::Serializer::ArraySerializer.new(results[0], each_serializer: ProjectSerializer, meta: results[1])
+    @map_data = Rails.cache.fetch("map_data_projects_#{digest}", :expires_in => 24.hours) {ActiveModel::Serializer::Adapter::JsonApi.new(m, include: ['organization', 'sectors', 'donors', 'geolocations']).to_json.to_s}
     @map_data_max_count = 0;
     @projects_count = Rails.cache.fetch("projects_count_#{digest}", :expires_in => 24.hours) {results[0].uniq.length}.to_f
     @projects = Rails.cache.fetch("projects_#{digest}", :expires_in => 24.hours) {results[0].page(params[:page]).per(10)}
