91f5d76d810d2cde7ba5a235c717e7baa492f685
Miguel Mendoza <info@miguelmendoza.dj>
2015-09-04 12:42:53 +0200

    removes cache

--- app/controllers/concerns/projects_filtering.rb
+++ app/controllers/concerns/projects_filtering.rb
@@ -1,22 +1,21 @@
 module ProjectsFiltering
   extend ActiveSupport::Concern
-  require 'digest/sha1'
+  # require 'digest/sha1'
   included do
     before_action :merge_params,  only: [:home, :show]
     before_action :get_projects,  only: [:home, :show]
   end
   def get_projects
-    timestamp = Project.order('updated_at desc').first.updated_at.to_s
-    string = timestamp + projects_params.inspect
-    digest = Digest::SHA1.hexdigest(string)
+    # timestamp = Project.order('updated_at desc').first.updated_at.to_s
+    # string = timestamp + projects_params.inspect
+    # digest = Digest::SHA1.hexdigest(string)
     results = Project.fetch_all(projects_params, false)
     m = ActiveModel::Serializer::ArraySerializer.new(results[0], each_serializer: ProjectSerializer, meta: results[1])
-    @map_data = Rails.cache.fetch("map_data_projects_#{digest}", :expires_in => 24.hours) {ActiveModel::Serializer::Adapter::JsonApi.new(m, include: ['organization', 'sectors', 'donors', 'geolocations']).to_json.to_s}
-    @map_data_max_count = 0;
-    @projects_count = Rails.cache.fetch("projects_count_#{digest}", :expires_in => 24.hours) {results[0].uniq.length}.to_f
-    @projects = Rails.cache.fetch("projects_#{digest}", :expires_in => 24.hours) {results[0].page(params[:page]).per(10)}
+    @map_data = ActiveModel::Serializer::Adapter::JsonApi.new(m, include: ['organization', 'sectors', 'donors', 'geolocations']).to_json.to_s
+    @map_data_max_count = 0
+    @projects_count = results[0].uniq.length.to_f
+    @projects = results[0].page(params[:page]).per(10)
     puts "***********************************************************************************************"
-    puts @projects
   end
   private
   def projects_params
