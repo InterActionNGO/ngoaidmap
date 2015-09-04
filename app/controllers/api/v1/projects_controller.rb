27598eba9a8de6623062148cbca81aadc0f3862b
Miguel Mendoza <info@miguelmendoza.dj>
2015-09-04 11:57:51 +0200

    testing api cache

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -1,10 +1,16 @@
 module Api
   module V1
+    require 'digest/sha1'
     class ProjectsController < ApiController
       def index
-        @projects = Project.fetch_all(project_params)
+        @projects = Project.fetch_all(projects_params)
+        timestamp = @projects.order('projects.updated_at desc').first.try('projects.updated_at').to_s
+        string = timestamp + projects_params.inspect
+        digest = Digest::SHA1.hexdigest(string)
+        m = ActiveModel::Serializer::ArraySerializer.new(@projects, each_serializer: ProjectSerializer)
+        @map_data = Rails.cache.fetch("map_data_projects_#{digest}", :expires_in => 24.hours) {ActiveModel::Serializer::Adapter::JsonApi.new(m, include: ['organization', 'sectors', 'donors', 'geolocations']).to_json}
         respond_to do |format|
-         format.json {render json: @projects, root: 'data', include: ['organization', 'sectors', 'donors', 'geolocations']}
+         format.json {render json: @map_data}
          format.xml {@projects}
         end
       end
@@ -18,7 +24,7 @@ module Api
       end
 
 
-      def project_params
+      def projects_params
         params.permit(:offset, :limit, :format, :geolocation, :level, organizations:[], sectors:[], donors:[], countries:[])
       end
     end
