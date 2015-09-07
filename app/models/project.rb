0c9c704d52bb13fc52f799532df9e0fdefa5ce37
Miguel Mendoza <info@miguelmendoza.dj>
2015-09-07 17:01:59 +0200

    adds project resources

--- app/models/project.rb
+++ app/models/project.rb
@@ -59,7 +59,7 @@ class Project < ActiveRecord::Base
   has_and_belongs_to_many :tags
   has_and_belongs_to_many :geolocations
   has_many :resources, -> {where(element_type: 0)}, :foreign_key => :element_id, :dependent => :destroy
-  #has_many :media_resources, :conditions => proc {"media_resources.element_type = #{Iom::ActsAsResource::PROJECT_TYPE}"}, :foreign_key => :element_id, :dependent => :destroy, :order => 'position ASC'
+  has_many :media_resources, -> {where(element_type: 0).order('position ASC')}, :foreign_key => :element_id, :dependent => :destroy
   has_many :donations, :dependent => :destroy
   has_many :donors, :through => :donations
   has_many :cached_sites, :class_name => 'Site'#, :finder_sql => 'select sites.* from sites, projects_sites where projects_sites.project_id = #{id} and projects_sites.site_id = sites.id'
