d9dd050d344a6203388c5f04788eea6b10188c73
David Inga <david.inga@vizzuality.com>
2015-09-08 16:25:30 +0200

    added assets on precompile

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,5 +9,6 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( application/*.js )
 Rails.application.config.assets.precompile += %w( application/main.js )
 Rails.application.config.assets.precompile += %w( main.css )
