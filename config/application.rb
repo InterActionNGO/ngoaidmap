937f4595260cbe8c02731167239b1694402f95b9
David Inga <david.inga@vizzuality.com>
2015-09-15 16:23:24 +0200

    fix deploy

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,6 @@ module NgoFront
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    config.assets.paths << File.join(Rails.root, 'public', 'app', 'lib')
     config.assets.paths << File.join(Rails.root, 'public', 'app', 'vendor')
 
     config.generators.assets = false
