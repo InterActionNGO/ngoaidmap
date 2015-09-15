e6812e36edf53a1e68191834c83ef31f61e5cfc7
Miguel Mendoza <info@miguelmendoza.dj>
2015-09-15 13:44:35 +0200

    searches refactor

--- app/controllers/concerns/preload_vars.rb
+++ app/controllers/concerns/preload_vars.rb
@@ -4,7 +4,6 @@ module PreloadVars
     before_action :get_menu_items
   end
   def get_menu_items
-    #@projects = Project.active
     @organizations = Organization.active.order(:name).uniq.pluck(:id, :name)
     @donors = Donor.active.order(:name).uniq.pluck(:id, :name)
     @countries = Geolocation.where(adm_level: 0).order(:name).uniq.pluck(:uid, :name)
