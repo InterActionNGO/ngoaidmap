7602eb0cc0b134418e5dde2098c0f38460f2b207
Miguel Mendoza <info@miguelmendoza.dj>
2015-09-22 15:00:38 +0200

    fix for active projects in menu

--- app/assets/javascripts/application/services/sidebarService.js
+++ app/assets/javascripts/application/services/sidebarService.js
@@ -12,8 +12,8 @@ define([
   var SidebarService = Class.extend({
 
     requests: {
-      'sectors-all' : '/api/sectors?include=projects_count&status=true',
-      'countries-all' : '/api/countries?summing=projects&status=true'
+      'sectors-all' : '/api/sectors?include=projects_count&status=active',
+      'countries-all' : '/api/countries?summing=projects&status=active'
     },
 
     /**
@@ -44,11 +44,11 @@ define([
 
     _defineRequestsByPlace: function(){
       if (!!window.sector) {
-        this.requests['donors-by-sector'] = _.str.sprintf('/api/donors?sectors[]=%s&status=true', sector.id)
+        this.requests['donors-by-sector'] = _.str.sprintf('/api/donors?sectors[]=%s&status=active', sector.id)
       }
       if (!!window.geolocation) {
-        this.requests['breadcrumbs'] = _.str.sprintf('/api/geolocations/%s?get_parents=true&status=true', geolocation.uid);
-        this.requests['donors-by-geolocation'] = _.str.sprintf('/api/donors?geolocation=%s&status=true', geolocation.uid);
+        this.requests['breadcrumbs'] = _.str.sprintf('/api/geolocations/%s?get_parents=active&status=active', geolocation.uid);
+        this.requests['donors-by-geolocation'] = _.str.sprintf('/api/donors?geolocation=%s&status=active', geolocation.uid);
       }
     },
 
