252097b1162089726d6135a3bdf0d3cec5c3c432
Miguel Mendoza <info@miguelmendoza.dj>
2015-10-08 18:24:19 +0200

    iati cahnges

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -4,8 +4,14 @@ module Api
       def index
         @projects = Project.fetch_all(projects_params)
         respond_to do |format|
-         format.json {render json: @projects, root: 'data', include: ['organization', 'sectors', 'donors']}
-         format.xml {@projects}
+          format.json {render json: @projects, root: 'data', include: ['organization', 'sectors', 'donors']}
+          format.xml {
+            no_limit_params = projects_params.dup
+            no_limit_params.delete(:offset)
+            no_limit_params.delete(:limit)
+            @total_projects = Project.fetch_all(no_limit_params).size
+            @projects
+          }
         end
       end
 
@@ -19,10 +25,10 @@ module Api
 
 
       def projects_params
-        if request.fullpath.include?('iati') && (!params[:limit] or params[:limit].to_i > 100)
-          params.merge!(limit: '10')
+        if request.fullpath.include?('iati') && ((!params[:limit].present? && !params[:organizations].present? && !params[:sectors].present? && !params[:donors].present? && !params[:countries].present?) or params[:limit].to_i > 100)
+          params.merge!(limit: '100')
         end
-        params.permit(:offset, :limit, :format, :status, :geolocation, :starting_after, :ending_before, :q, :level, organizations:[], sectors:[], donors:[], countries:[])
+        params.permit(:offset, :limit, :status, :geolocation, :starting_after, :ending_before, :q, :level, organizations:[], sectors:[], donors:[], countries:[])
       end
     end
   end
