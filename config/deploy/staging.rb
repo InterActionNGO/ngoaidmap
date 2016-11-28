050c83b182ebeff6ef723a6e90f5834f456cabef
Brian Landau <brianjlandau@gmail.com>
2016-11-28 13:11:30 -0700

    Update node version

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -13,7 +13,7 @@ role :db,  %w{deploy@198.199.86.239}
 set :ssh_options, {:forward_agent => true}
 set :linked_files, %w{.env config/database.yml config/secrets.yml}
 
-set :bower_bin, '/usr/local/nvm/versions/node/v4.3.1/bin/bower'
+set :bower_bin, '/usr/local/nvm/versions/node/v0.10.29/bin/bower'
 
 server '198.199.86.239', user: 'deploy', roles: [:web, :db, :app]
 
