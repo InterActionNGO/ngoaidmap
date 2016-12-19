6ce68bfe34b1321b5d8c3fa3353c200eadefe8cf
Megan Zlock <zlockart@gmail.com>
2016-12-19 10:54:21 -0500

    add context to org links on location and section pages

--- app/assets/javascripts/application/views/sidebar/sidebarOrganizations.js
+++ app/assets/javascripts/application/views/sidebar/sidebarOrganizations.js
@@ -24,8 +24,39 @@ define([
       this.conexion = options.conexion;
       this.conexion.getOrganizationsData(_.bind(function(response){
         this.organizations = response.organizations_count;
+        this.updateOrgUrls();
         this.render(false);
       }, this ));
+    },
+
+    updateOrgUrls: function() {
+      var new_orgs = [];
+      for (var i=0, len=this.organizations.length; i < len; i++) {
+        var org = this.organizations[i];
+        var url = org.url + this.getUrlContext();
+
+        org.url = url;
+        new_orgs.push(org);
+      }
+
+      this.organizations = new_orgs;
+    },
+
+    getUrlContext: function() {
+      var url = window.location;
+      var path_array = window.location.pathname.split( '/' );
+      var context = '?';
+
+      if (path_array[1] === 'location') {
+        context += 'geolocation=';
+        context += path_array[2];
+        context += '&level=1'
+      } else if (path_array[1] === 'sectors') {
+        context += 'sectors[]=';
+        context += path_array[2];
+      }
+
+      return context;
     },
 
     parseData: function(more){
