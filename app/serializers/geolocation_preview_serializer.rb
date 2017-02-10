ebbcce6b3020ea3e772ff060724633077c7eedba
Robert Burnett <robert@opuselegans.com>
2017-02-10 17:56:07 -0500

    pare down geolocation_preview_serializer

--- app/serializers/geolocation_preview_serializer.rb
+++ app/serializers/geolocation_preview_serializer.rb
@@ -1,46 +1,11 @@
 class GeolocationPreviewSerializer < ActiveModel::Serializer
-  cache key: "main_api_geolocation_preview", expires_in: 3.hours
-  attributes :id, :vocab_id, :name, :center_latitude, :center_longitude, :country_iso2_code, :country_name, :country_vocab_id, :data_provider, :admin_level, :adm0_relation_vocab_id, :adm1_relation_vocab_id, :adm2_relation_vocab_id, :adm3_relation_vocab_id, :adm4_relation_vocab_id, :custom_geo_source, :record_updated_at, :record_created_at
-  def vocab_id
-    object.uid
-  end
-  def center_latitude
-    object.latitude
-  end
-  def center_longitude
-    object.longitude
-  end
-  def country_iso2_code
-    object.country_code
-  end
-  def country_vocab_id
-    object.country_uid
-  end
-  def data_provider
-    object.provider
-  end
-  def admin_level
-    object.adm_level
-  end
-  def adm0_relation_vocab_id
-    object.g0
-  end
-  def adm1_relation_vocab_id
-    object.g1
-  end
-  def adm2_relation_vocab_id
-    object.g2
-  end
-  def adm3_relation_vocab_id
-    object.g3
-  end
-  def adm4_relation_vocab_id
-    object.g4
-  end
-  def record_updated_at
-    object.updated_at
-  end
-  def record_created_at
-    object.created_at
-  end
+    cache key: "main_api_geolocation_preview", expires_in: 3.hours
+    attribute :name
+    attribute :adm_level, key: :admin_level
+    attribute :latitude, key: :center_latitude
+    attribute :longitude, key: :center_longitude
+    link :self do
+        api_geolocation_path(object)
+    end
 end
+
