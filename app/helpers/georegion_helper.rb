74f0d1d2c23feabc764590fa803d647c97a3615d
Robert Burnett <robert@opuselegans.com>
2017-03-07 21:44:55 -0500

    single filter titles updated with new style/content

--- app/helpers/georegion_helper.rb
+++ app/helpers/georegion_helper.rb
@@ -1,10 +1,19 @@
 module GeoregionHelper
-  def georegion_projects_list_subtitle
-    if @filter_by_category
-      pluralize(@georegion_projects_count, "#{@category_name} project", "#{@category_name} projects") + " in #{@area.name.capitalize}"
-    else
-      "#{pluralize(@georegion_projects_count, 'project', 'projects')} in #{@area.name.capitalize}"
-    end
-  end
+  # def georegion_projects_list_subtitle
+  #   if @filter_by_category
+  #     pluralize(@georegion_projects_count, "#{@category_name} project", "#{@category_name} projects") + " in #{@area.name.capitalize}"
+  #   else
+  #     "#{pluralize(@georegion_projects_count, 'project', 'projects')} in #{@area.name.capitalize}"
+  #   end
+  # end
 
+  def location_projects_subtitle
+    this_level = @geolocation.adm_level
+    available_levels = [4,3,2,1,0]
+    names = []
+    available_levels.each do |l|
+      names << Geolocation.find_by(:uid => @geolocation["g#{l}"]).try(:name) || nil if this_level >= l+1
+    end
+    names.compact.join(', ')
+  end
 end
