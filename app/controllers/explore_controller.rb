acd0e9757ff51f489c72df2616c9ee22b0b607fd
Robert Burnett <robert@opuselegans.com>
2017-05-30 15:31:40 -0400

    set top-level menu links and routes

--- app/controllers/explore_controller.rb
+++ app/controllers/explore_controller.rb
@@ -1,13 +1,16 @@
 class ExploreController < ApplicationController
    layout 'explore_layout'
     def index
+    end
+
+    def stories
+    end
+
+    def data
         @organizations = Organization.joins(:projects).select('distinct organizations.id, organizations.name,organizations.logo_file_name, max(projects.created_at) as max').group(:id,:name,:logo_file_name).limit(25).order('max desc').all
         
         @sectors = Sector.joins(:projects).select('distinct sectors.id, sectors.name, max(projects.created_at) as max').group(:id, :name).limit(25).order('max desc').all
         
         @countries = Geolocation.joins(:projects).select('distinct geolocations.country_name, max(projects.created_at) as max').group(:country_name).limit(25).order('max desc').all
-    end
-
-    def stories
     end
 end
