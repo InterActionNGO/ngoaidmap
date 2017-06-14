6be1c37789d588c6b01b0884fc318d152615590f
Eli Fatsi <eli.fatsi@viget.com>
2017-06-14 15:31:16 -0600

    Split Partners nav item into local/international

--- app/controllers/concerns/preload_vars.rb
+++ app/controllers/concerns/preload_vars.rb
@@ -4,10 +4,12 @@ module PreloadVars
     before_action :get_menu_items
   end
   def get_menu_items
-    @organizations = Organization.site(@site.id).active.order(:name).uniq.pluck(:id, :name)
-    @donors = Organization.with_donations.donated_projects_site(@site.id).active_donated_projects.uniq.order(:name).uniq.pluck(:id, :name)
-    @countries = Geolocation.where(adm_level: 0).order(:name).uniq.pluck(:uid, :name)
-    @sectors = Sector.counting_projects(status: 'active', site: @site.id)
-    @partners = Organization.joins(:partner_projects).merge(Project.active).order(:name).uniq.pluck(:id, :name)
+    @organizations          = Organization.site(@site.id).active.order(:name).uniq.pluck(:id, :name)
+    @donors                 = Organization.with_donations.donated_projects_site(@site.id).active_donated_projects.uniq.order(:name).uniq.pluck(:id, :name)
+    @countries              = Geolocation.where(adm_level: 0).order(:name).uniq.pluck(:uid, :name)
+    @sectors                = Sector.counting_projects(status: 'active', site: @site.id)
+    @partners               = Organization.joins(:partner_projects).merge(Project.active).order(:name).uniq.pluck(:id, :name)
+    @local_partners         = Organization.local.joins(:partner_projects).merge(Project.active).order(:name).uniq.pluck(:id, :name)
+    @international_partners = Organization.international.joins(:partner_projects).merge(Project.active).order(:name).uniq.pluck(:id, :name)
   end
 end
