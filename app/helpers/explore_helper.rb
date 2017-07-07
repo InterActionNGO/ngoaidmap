42ccec6ee10e840fa13e31ecde5a39239439abd0
Megan Zlock <zlockart@gmail.com>
2017-07-07 15:11:37 -0400

    Revert "revisions to explore section"
    This reverts commit a4c3c98d414800e7a4e66e4947529496318151d2.

--- app/helpers/explore_helper.rb
+++
@@ -1,8 +0,0 @@
-module ExploreHelper
-
-  def explore_page_title
-      return "What People Are Saying About NGO Aid Map" if action_name.eql?('stories')
-      return "Discover Data Beyond The Map" if action_name.eql?('data')
-  end
-
-end
