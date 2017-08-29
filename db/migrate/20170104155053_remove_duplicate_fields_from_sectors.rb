8d4c82c46b333298d270ef18fc22f330dd447e8b
David Eisinger <david.eisinger@viget.com>
2017-08-29 15:53:47 -0400

    Remove problematic migration
    Fields were already renamed in db/migrate/20150923164350_rename_sector_fields.rb

--- db/migrate/20170104155053_remove_duplicate_fields_from_sectors.rb
+++
@@ -1,6 +0,0 @@
-class RemoveDuplicateFieldsFromSectors < ActiveRecord::Migration
-  def change
-      remove_column :sectors, :oec_dac_name
-      remove_column :sectors, :oec_dac_purpose_code
-  end
-end
