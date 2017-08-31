bdf2fcb19cbe5b91e5dd8ccf7397ebaeb7512a1e
David Eisinger <david.eisinger@viget.com>
2017-08-31 16:58:42 -0400

    Fix HumanitarianScope factory

--- spec/factories/humanitarian_scopes.rb
+++ spec/factories/humanitarian_scopes.rb
@@ -3,8 +3,6 @@ FactoryGirl.define do
     project
     humanitarian_scope_type
     humanitarian_scope_vocabulary
-    vocabulary_uri "MyString"
-    code "MyString"
     narrative "MyString"
   end
 end
