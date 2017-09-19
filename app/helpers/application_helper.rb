48641d1f6b1f8410f0799fa1299d76c7934dde41
Robert Burnett <robert@opuselegans.com>
2017-09-19 16:24:53 -0400

    add hover style and links to roles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -386,19 +386,22 @@ HTML
             class_bg: 'organization-bg',
             'class': 'organization',
             label: 'Reporting Organization',
-            count: Organization.find(request[:id].to_i).projects.active.size
+            count: Organization.find(request[:id].to_i).projects.active.size,
+            url: organization_path(request[:id])
         }
         partner = {
             class_bg: 'partner-bg',
             'class': 'partner',
             label: 'Partner',
-            count: Organization.active.partners(request[:id].to_i).size
+            count: Organization.active.partners(request[:id].to_i).size,
+            url: partner_path(request[:id])
         }
         donor = {
             class_bg: 'donor-bg',
             'class': 'donor',
             label: 'Donor',
-            count: Organization.active.donors(request[:id].to_i).size
+            count: Organization.active.donors(request[:id].to_i).size,
+            url: donor_path(request[:id])
         }
         arr = []
         if controller_name == 'organizations'
