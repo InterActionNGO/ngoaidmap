f737eab1a74ef863ff5868d8063bcc097cfd99de
Robert Burnett <robert@opuselegans.com>
2018-02-12 11:17:29 -0500

    update bower bin for capistrano

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -8,7 +8,7 @@ role :db,  %w{deploy@52.179.82.220}
 set :ssh_options, {:forward_agent => true}
 set :linked_files, %w{.env config/database.yml config/secrets.yml}
 
-set :bower_bin, '/home/deploy/.nvm/v0.10.29/lib/node_modules/bower'
+set :bower_bin, '/home/deploy/.nvm/v0.10.29/bin/bower'
 
 server '52.179.82.220', user: 'deploy', roles: [:web, :db, :app]
 
