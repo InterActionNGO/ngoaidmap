10e65477c7818832f2a4c76999ef1a5ff289c3a3
Robert Burnett <robert@opuselegans.com>
2018-03-20 14:06:39 -0400

    Revert "customize rake job definition in whenever"
    This reverts commit acae3a3b10b52f3734800d2fb239312363d7c6ba.

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,3 @@
-# Customize the rake job to source user profile for environment variables
-job_type :rake, ". $HOME/.profile; cd :path && :environment_variable=:environment bundle exec rake :task --silent :output"
-
 set :bundle_command, "/home/deploy/.rbenv/versions/2.3.1/bin/bundle exec"
 set :output, { error: '/var/www/shared/cron.front.error.log', standard: '/var/www/shared/cron.front.log' }
 every 1.day, :at => '1:30 am' do
