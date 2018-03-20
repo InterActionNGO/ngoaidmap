21d61b61ced9755338694fde0860238ddcbf171a
Robert Burnett <robert@opuselegans.com>
2018-03-20 13:33:41 -0400

    add env var to whenever config

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,7 @@
 set :bundle_command, "/home/deploy/.rbenv/versions/2.3.1/bin/bundle exec"
 set :output, { error: '/var/www/shared/cron.front.error.log', standard: '/var/www/shared/cron.front.log' }
+env :MAILCHIMP_API_KEY, ENV['MAILCHIMP_API_KEY']
+
 every 1.day, :at => '1:30 am' do
   rake "iom:update_sites"
   rake "iom:update_tag_project_counts"
